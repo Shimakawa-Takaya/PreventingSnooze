0fc591f158922424abdb6f69245fc3f712501743
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2017-04-25 03:24:00 +0900

    make model & view & presenter directory

--- app/src/main/java/com/takaya_shimakawa/prevent_over_sleep/view/MainActivity.java
+++ app/src/main/java/com/takaya_shimakawa/prevent_over_sleep/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.takaya_shimakawa.prevent_over_sleep;
+package com.takaya_shimakawa.prevent_over_sleep.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.takaya_shimakawa.prevent_over_sleep.R;
 
 public class MainActivity extends AppCompatActivity {
 
