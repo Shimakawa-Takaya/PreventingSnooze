0e174b6f87fe29162b7a3d4a1a5263208ea290a8
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2017-04-27 13:19:45 +0900

    mearge test

--- app/src/main/java/com/takaya_shimakawa/prevent_over_sleep/view/MainActivity.java
+++ app/src/main/java/com/takaya_shimakawa/prevent_over_sleep/view/MainActivity.java
@@ -2,6 +2,8 @@ package com.takaya_shimakawa.prevent_over_sleep.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.takaya_shimakawa.prevent_over_sleep.R;
 
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_station);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, STATIONS);
+        textView.setAdapter(adapter);
     }
+
+
+
+    static final String[] STATIONS = new String[]{
+            "A","B","c","d"
+    };
 }
+
